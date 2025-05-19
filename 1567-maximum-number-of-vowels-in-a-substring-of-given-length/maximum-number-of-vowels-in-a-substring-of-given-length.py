4447675641614464cf952e44e4d8fb8e26be23da
ChanYenHsiung <167671866+Chanyen20@users.noreply.github.com>
2025-05-18 20:06:09 -0400

    Time: 103 ms (31.44%) | Memory: 17.9 MB (97.93%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,22 +1,25 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
-        vowels = {'a','e','i','o','u'}  
+        vowels = ['a','e','i','o','u'] 
         maxNumVowel = 0
-        left = 0
 
-        for i in range(0, k):
-            if s[i] in vowels:
-                maxNumVowel += 1
-        currentNum = maxNumVowel
+        # for i in range(0, k):
+        #     if s[i] in vowels:
+        #         maxNumVowel += 1
+        currentNum = sum(1 for ch in s[:k] if ch in vowels)
+        maxNumVowel = currentNum
 
         for right in range(k, len(s)):
-            if not (s[left] in vowels) and s[right] in vowels:
+            # if not (s[left] in vowels) and s[right] in vowels:
+            #     currentNum += 1
+            # elif s[left] in vowels and not (s[right] in vowels):
+            #     currentNum -= 1
+            if s[right] in vowels:
                 currentNum += 1
-            elif s[left] in vowels and not (s[right] in vowels):
+            if s[right - k] in vowels:
                 currentNum -= 1
-            
+
             maxNumVowel = max(maxNumVowel, currentNum)
-            left += 1
 
         return maxNumVowel
         
