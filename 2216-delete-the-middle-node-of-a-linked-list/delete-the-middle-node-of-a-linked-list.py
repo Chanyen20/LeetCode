6d2ac18682623ba29924a30d5034324f41cd82ea
ChanYenHsiung <167671866+Chanyen20@users.noreply.github.com>
2025-06-16 16:17:18 -0400

    Time: 52 ms (66.28%) | Memory: 49.3 MB (23.00%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
@@ -5,27 +5,19 @@
 #         self.next = next
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # fast 每次走 2 步
-        # slow 每次走 1 步
         if head is None or head.next is None:
             return None
-
         slow = head
-        fast = head
-        prev = None  # 記錄 slow 的前一個節點
+        fast = head.next.next
 
         while fast and fast.next:
-            prev = slow
             slow = slow.next
             fast = fast.next.next
-
-        # 此時 slow 指向中間節點，prev 是它的前一個
-        prev.next = slow.next  # 刪除中間節點
+        
+        slow.next = slow.next.next
 
         return head
 
 
 
 
-# Time: O(n)
-# Space: O(1)
