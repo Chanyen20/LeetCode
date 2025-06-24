77a9061148eb8d1a7dbc1b63d69894f23e000823
ChanYenHsiung <167671866+Chanyen20@users.noreply.github.com>
2025-06-24 15:10:22 -0400

    Time: 14 ms (73.41%) | Memory: 37.5 MB (23.65%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
@@ -5,14 +5,17 @@ class Solution:
 
         for num in nums:
             if num <= first:
-                first = num  # 更新最小值
+                first = num
             elif num <= second:
-                second = num  # 找到比 first 大的第二小值
+                second = num
             else:
-                # 找到比 second 還大的數，即存在 triplet
                 return True
 
         return False
+            
+        
+
+# [1,2,3,4,5]
 
 
 
