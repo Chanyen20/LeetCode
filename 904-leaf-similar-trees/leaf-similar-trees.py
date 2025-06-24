72f85bf1ff8d245b16c103afd74195383af26c65
ChanYenHsiung <167671866+Chanyen20@users.noreply.github.com>
2025-06-24 17:58:09 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (44.57%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.py
+++ 904-leaf-similar-trees/leaf-similar-trees.py
@@ -11,13 +11,13 @@ class Solution:
         self.helper(root1, leaf1)
         self.helper(root2, leaf2)
         return leaf1 == leaf2
-
     def helper(self, root, leaf):
         if not root:
-            return leaf
-        if not root.right and not root.left:
+            return
+        if not root.left and not root.right:
             leaf.append(root.val)
-
+            return
         self.helper(root.left, leaf)
         self.helper(root.right, leaf)
+
         
