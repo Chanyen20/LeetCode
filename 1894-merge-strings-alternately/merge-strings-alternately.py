48fa545a7d99cecccd8ab0936e801d1c536d5177
ChanYenHsiung <167671866+Chanyen20@users.noreply.github.com>
2025-06-23 22:56:14 -0400

    Time: 41 ms (36.80%) | Memory: 17.9 MB (33.71%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,11 +1,26 @@
+# class Solution:
+#     def mergeAlternately(self, word1: str, word2: str) -> str:
+#         res = ''
+#         for i in range(max(len(word1), len(word2))):
+#             if i < len(word1):
+#                 res = res + word1[i]
+#             if i < len(word2):
+#                 res = res + word2[i]
+
+#         return res
+
+# time complexity:  0 < O(n) n < len(word1), len(word2))
+# space complexity: 0 < O(n) n < len(word1), len(word2))
+
+# Python 中字串是 immutable 的，每次用 res + ... 其實都會產生新字串。如果你要處理很長的字串，建議這樣寫
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        res = ''
-
+        res = []
         for i in range(max(len(word1), len(word2))):
             if i < len(word1):
-                res = res + word1[i]
+                res.append(word1[i])
             if i < len(word2):
-                res = res + word2[i]
-        
-        return res
+                res.append(word2[i])
+        return ''.join(res)
+
+
