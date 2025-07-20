f9ca1107620fef72615b959cac5101da7053645e
ChanYenHsiung <167671866+Chanyen20@users.noreply.github.com>
2025-07-20 16:00:38 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (71.15%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,11 +1,13 @@
 import math
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        # edge case
-        if str1 + str2 != str2 + str1:
-            return ""
-        
-        gecLength = math.gcd(len(str1), len(str2))
-        return str1[:gecLength]
+        shortest_len = math.gcd(len(str1), len(str2))
 
+        if str1[:shortest_len] == str2[:shortest_len] and str1 + str2 == str2 + str1:
+            return str1[:shortest_len]
+        else:
+            return ""
+
+# time: O(n)
+# space: O(1)
         
