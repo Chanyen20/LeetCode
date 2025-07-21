39959949af2e3dcb6217610787b7c1c6ae3eb053
ChanYenHsiung <167671866+Chanyen20@users.noreply.github.com>
2025-07-21 18:42:01 -0400

    Time: 11 ms (39.93%) | Memory: 18.1 MB (54.59%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -1,22 +1,21 @@
-from typing import List
-
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         pointer = 0
-        length = len(flowerbed)
-
-        while pointer < length:
+        
+        while pointer < len(flowerbed):
             if flowerbed[pointer] == 0:
-                empty_left = (pointer == 0 or flowerbed[pointer - 1] == 0)
-                empty_right = (pointer == length - 1 or flowerbed[pointer + 1] == 0)
-
-                if empty_left and empty_right:
-                    # flowerbed[pointer] = 1
-                    n -= 1
-                    if n == 0:
-                        return True
+                left_empty = (pointer == 0 or flowerbed[pointer - 1] == 0)
+                right_empty = (pointer == len(flowerbed) - 1 or flowerbed[pointer + 1] == 0)
+                
+                if left_empty and right_empty:
                     pointer += 2
-                    continue
-            pointer += 1
-
+                    n -= 1
+                else:
+                    pointer += 1
+            else:
+                pointer += 1
+        
         return n <= 0
+
+        
+        
