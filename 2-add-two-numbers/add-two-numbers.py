58b6e0fe6747bcf5f7303100cb53717e4890a11f
ChanYenHsiung <167671866+Chanyen20@users.noreply.github.com>
2025-12-27 18:10:02 -0500

    Time: 0 ms (100.00%) | Memory: 17.5 MB (88.45%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -3,31 +3,49 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        s1 = self.reverse_and_str(l1)
-        s2 = self.reverse_and_str(l2)
-        res_num = int(s1) +  int(s2) 
-        res = self.turnToListNode(res_num)
-        return res 
+        dummy_node = ListNode(0)
+        curr = dummy_node
+        temp = 0
 
-    def reverse_and_str(self, listNode):
-        curr = listNode
-        s = ''
-        while curr:
-            s += str(curr.val)
-            curr = curr.next
-        return s[::-1]
-    
-    def turnToListNode(self, res_num):
-        dummy = ListNode()
-        curr = dummy
-        for ch in str(res_num)[::-1]:
-            curr.next = ListNode(int(ch))
-            curr = curr.next
-        return dummy.next
+        while l1 or l2:
+            if l1 and l2:
+                value = l1.val + l2.val + temp
+                curr.next = ListNode(value % 10)
+                curr = curr.next
+                temp = value // 10
+                l1 = l1.next
+                l2 = l2.next
+                continue
+            if l1 and not l2:
+                value = l1.val + temp
+                curr.next = ListNode(value % 10)
+                curr = curr.next
+                temp = value // 10
+                l1 = l1.next
+                continue
+            if not l1 and l2:
+                value = l2.val + temp
+                curr.next = ListNode(value % 10)
+                curr = curr.next
+                temp = value // 10
+                l2 = l2.next
+                continue
+            
+        if temp != 0:
+            curr.next = ListNode(temp)
 
+        return dummy_node.next
+
+# l1 = [2, 4, 3]
+#          |
+# l2 = [5, 6, 4, 6]
+#          |
+
+
+
+            
 
 
 
