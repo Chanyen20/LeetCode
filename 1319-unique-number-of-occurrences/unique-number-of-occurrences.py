9a46c6656d32ff1386e2396ba3b3dfa0c6a8a4c3
ChanYenHsiung <167671866+Chanyen20@users.noreply.github.com>
2026-01-13 19:27:31 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (9.85%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -1,27 +1,14 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        arr.sort()
+        seen = {}
 
-        cnt = 0
-        seen_cnt = set()
-
-        for i in range(len(arr)):
-            if i == 0:
-                cnt += 1
-                continue
-            
-            if arr[i] == arr[i - 1]:
-                cnt += 1
+        for num in arr:
+            if num not in seen:
+                seen[num] = 1
             else:
-                if cnt in seen_cnt:
-                    return False
-                seen_cnt.add(cnt)
-                cnt = 1
-            
-        if cnt in seen_cnt:
-            return False
-        return True
+                seen[num] += 1
         
+        return len(seen) == len(set(seen.values()))
 
 
         
