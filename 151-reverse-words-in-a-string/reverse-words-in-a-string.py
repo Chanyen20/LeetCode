be98247e6bc6f7885c6709a9547bf7a4ea78177c
ChanYenHsiung <167671866+Chanyen20@users.noreply.github.com>
2026-01-17 00:22:35 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (9.04%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,7 +1,16 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        s_list = s.split()
+        words = s.split()
+        res = []
+        pointer = len(words) - 1
+
+        while pointer >= 0:
+           res.append(words[pointer])
+           pointer -= 1
         
+        return ' '.join(res)
+
+
         
-        return ' '.join(s_list[::-1])
+      
         
