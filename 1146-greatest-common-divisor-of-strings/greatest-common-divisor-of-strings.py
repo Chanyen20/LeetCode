965e1370af127ad9e90039fe28fb101c16ef6110
ChanYenHsiung <167671866+Chanyen20@users.noreply.github.com>
2026-01-16 20:51:56 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (14.28%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,13 +1,8 @@
-# import math
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        shortest_len = math.gcd(len(str1), len(str2))
-
-        if str1[:shortest_len] == str2[:shortest_len] and str1 + str2 == str2 + str1:
-            return str1[:shortest_len]
-        else:
+        if str1 + str2 != str2 + str1:
             return ""
 
-# time: O(n)
-# space: O(1)
-        
+        gcd = math.gcd(len(str1), len(str2))
+
+        return str1[:gcd]
