8363b6401fb4d972060e6939938cfc15767680a7
ChanYenHsiung <167671866+Chanyen20@users.noreply.github.com>
2026-02-06 12:18:00 -0500

    Time: 15 ms (42.64%) | Memory: 19.4 MB (41.33%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,6 +1,22 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # if the length is not the same, means they were not anagram
+        if len(s) != len(t):
+            return False
         
-        return sorted(s) == sorted(t)
-        
+        seen_s = dict()
+        seen_t = dict()
 
+        for s_char, t_char in zip(s, t):
+            if s_char not in seen_s:
+                seen_s[s_char] = 0
+            
+            if t_char not in seen_t:
+                seen_t[t_char] = 0
+            
+            seen_s[s_char] += 1
+            seen_t[t_char] += 1
+        
+        return seen_s == seen_t
+
+        
