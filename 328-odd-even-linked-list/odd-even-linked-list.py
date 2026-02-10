409e67f4a56b9a6c51ad5f40df6e44275d27a61c
ChanYenHsiung <167671866+Chanyen20@users.noreply.github.com>
2026-02-09 21:09:49 -0500

    Time: 0 ms (100.00%) | Memory: 21.2 MB (47.14%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.py
+++ 328-odd-even-linked-list/odd-even-linked-list.py
@@ -5,24 +5,23 @@
 #         self.next = next
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None or head.next is None:
-            return head
+        if not head:
+            return None
+        
         odd = head
         even = head.next
         even_head = even
 
-        while even is not None and even.next is not None:
-            odd.next = odd.next.next
+        while even and even.next:
+            odd.next = even.next
             odd = odd.next
-            even.next = even.next.next
-            even = even.next
-            
 
+            even.next = odd.next
+            even = even.next
+        
         odd.next = even_head
 
         return head
-       
-    
-    
-        return head
+
+
         
