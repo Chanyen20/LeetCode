2d37539752d005dcb2c7c56d939b1171d09c2eab
ChanYenHsiung <167671866+Chanyen20@users.noreply.github.com>
2026-02-25 23:52:33 -0500

    Time: 7 ms (89.76%) | Memory: 20.6 MB (47.32%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -1,20 +1,20 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        vowels = ['a', 'e', 'i', 'o','u', 'A', 'E', 'I', 'O','U']
-        s_list = list(s)
-        left_pointer, right_pointer = 0, len(s) - 1
+        vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+        seen_stack = []
+        res = []
 
+        for char in s:
+            if char in vowels:
+                seen_stack.append(char)
+        
+        for char in s:
+            if char in vowels:
+                replace = seen_stack.pop()
+                res.append(replace)
+            else:
+                res.append(char)
+    
+        return ''.join(res)
 
-        while left_pointer < right_pointer:
-            if s_list[left_pointer] not in vowels:
-                left_pointer += 1
-            if s_list[right_pointer] not in vowels:
-                right_pointer -= 1
-            if s_list[left_pointer] in vowels and s_list[right_pointer] in vowels:
-                s_list[left_pointer], s_list[right_pointer] = s_list[right_pointer], s_list[left_pointer]
-                left_pointer += 1
-                right_pointer -= 1
-        return ''.join(s_list)
-            
-
-
+        
