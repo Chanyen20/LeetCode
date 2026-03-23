71dda3a4a9f1dd6164076cbb052551de5511b96f
ChanYenHsiung <167671866+Chanyen20@users.noreply.github.com>
2026-03-23 08:13:52 +0800

    Time: 0 ms (100.00%) | Memory: 19.5 MB (15.04%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -4,8 +4,7 @@ class Solution:
         
         while pointer >= 0:
             if nums[pointer] == val:
-                nums[pointer], nums[-1] = nums[-1], nums[pointer]
-                del nums[-1]
+                del nums[pointer]
             pointer -= 1
         
         return len(nums)
